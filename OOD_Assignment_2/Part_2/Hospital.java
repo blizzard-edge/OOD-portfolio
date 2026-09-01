909d6e2310454eaf18b575d2c758e5273bb8fc49
Blizzard <278835811+blizzard-edge@users.noreply.github.com>
2026-09-01 17:37:34 -0400

    Implement Hospital class to manage unhealthy animals
    This class manages a list of unhealthy animals, allowing for their admission and release.

--- OOD_Assignment_2/Part_2/Hospital.java
+++ OOD_Assignment_2/Part_2/Hospital.java
@@ -1,1 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/*
+ * Author: Caitlin Hagler
+ * Class: Hospital
+ * Purpose: Stores animals that are currently unhealthy.
+ * Last Revision Date: September 1, 2026
+ */
+
+public class Hospital {
+
+    private final List<Animal> animals;
+
+    /*
+     * Creates an empty hospital.
+     */
+  
+    public Hospital() {
+        animals = new ArrayList<>();
+    }
+
+    /*
+     * Admits an unhealthy animal.
+     *
+     * @param animal animal being admitted
+     */
+  
+    public void admitAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    /*
+     * Releases an animal after it has recovered.
+     *
+     * @param animal animal being released
+     */
+  
+    public void releaseAnimal(Animal animal) {
+        animals.remove(animal);
+    }
+
+    /*
+     * Gets all animals currently in the hospital.
+     *
+     * @return unmodifiable list of hospital animals
+     */
+  
+    public List<Animal> getAnimals() {
+        return Collections.unmodifiableList(animals);
+    }
+}
