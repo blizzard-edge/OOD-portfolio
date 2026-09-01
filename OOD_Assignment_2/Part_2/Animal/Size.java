9518dbbd26b662c65b877cbb596f953b9aa277a2
Blizzard <278835811+blizzard-edge@users.noreply.github.com>
2026-09-01 16:07:28 -0400

    Create Size enum for animal size categories
    Added an enum to represent size categories for zoo animals.

--- OOD_Assignment_2/Part_2/Animal/Size.java
+++ OOD_Assignment_2/Part_2/Animal/Size.java
@@ -1,1 +1,13 @@
+/*
+ * Author: Caitlin Hagler
+ * Class: Size
+ * Purpose: Represents the size categories used by Zoo animals.
+ * Last Revision Date: September 1, 2026
+ */
 
+public enum Size {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    EXTRALARGE
+}
