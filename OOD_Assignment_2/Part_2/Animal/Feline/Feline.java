df7dbc20d80b25e3d856c32c190f2c96851cacdb
Blizzard <278835811+blizzard-edge@users.noreply.github.com>
2026-09-01 19:42:31 -0400

    Add roaming method to Feline class
    Implement roaming behavior for Feline class with a 50% chance to sleep.

--- OOD_Assignment_2/Part_2/Animal/Feline/Feline.java
+++ OOD_Assignment_2/Part_2/Animal/Feline/Feline.java
@@ -1,1 +1,36 @@
+/*
+ * Author: Caitlin Hagler
+ * Class: Feline
+ * Purpose: Abstract family class implementing feline-specific roaming.
+ * Last Revision Date: September 1, 2026
+ */
+
+public abstract class Feline extends Animal {
+
+    protected Feline(Size size, String typeName) {
+        super(size, typeName);
+    }
+
+    /*
+     * Felines have a 50% chance to sleep instead of roaming.
+     */
+  
+    @Override
+    public void roam() {
+
+        if (Math.random() < 0.50) {
+
+            System.out.println(
+                    getName() +
+                    " decided to sleep instead of roaming."
+            );
+
+            sleep();
+        }
+        else {
+            System.out.println(
+                    getName() + " is roaming."
+            );
+        }
+    }
 
