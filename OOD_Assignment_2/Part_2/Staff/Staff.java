125cd5e3c9dbe9a2054f298c5e3a20ab39adbe3a
Blizzard <278835811+blizzard-edge@users.noreply.github.com>
2026-09-01 17:57:50 -0400

    Create abstract Staff class with name generation
    Added abstract Staff class for Zoo employees with name generation.

--- OOD_Assignment_2/Part_2/Staff/Staff.java
+++ OOD_Assignment_2/Part_2/Staff/Staff.java
@@ -1,1 +1,31 @@
+/*
+ * Author: Caitlin Hagler
+ * Class: Staff
+ * Purpose: Abstract base class for Zoo employees.
+ * Last Revision Date: September 1, 2026
+ */
 
+public abstract class Staff {
+
+    private final String name;
+
+    /*
+     * Constructs a staff member and generates a unique name.
+     *
+     * @param roleName staff role
+     */
+  
+    protected Staff(String roleName) {
+        this.name = NameGenerator.nextStaffName(roleName);
+    }
+
+    /*
+     * Gets the staff member's name.
+     *
+     * @return staff name
+     */
+  
+    public String getName() {
+        return name;
+    }
+}
