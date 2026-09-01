8f3b65b79a9fd86a43a8684deb296a5e313dbae8
Blizzard <278835811+blizzard-edge@users.noreply.github.com>
2026-09-01 19:02:01 -0400

    Create abstract Canine class for roaming behavior
    Added an abstract Canine class with roaming behavior.

--- OOD_Assignment_2/Part_2/Animal/Canine/Canine.java
+++ OOD_Assignment_2/Part_2/Animal/Canine/Canine.java
@@ -1,1 +1,24 @@
+/*
+ * Author: Caitlin Hagler
+ * Class: Canine
+ * Purpose: Abstract family class implementing canine roaming behavior.
+ * Last Revision Date: September 1, 2026
+ */
 
+public abstract class Canine extends Animal {
+
+    protected Canine(Size size, String typeName) {
+        super(size, typeName);
+    }
+
+    /*
+     * Canines use the common roaming behavior.
+     */
+  
+    @Override
+    public void roam() {
+        System.out.println(
+                getName() + " is roaming."
+        );
+    }
+}
