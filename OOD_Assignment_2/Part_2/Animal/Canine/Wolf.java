56a09732ad31251842529cf9bf6db1768ca5940f
Blizzard <278835811+blizzard-edge@users.noreply.github.com>
2026-09-02 14:48:57 -0400

    Implement Wolf class extending Canine
    Added a Wolf class that extends Canine and implements a specific sound method.

--- OOD_Assignment_2/Part_2/Animal/Canine/Wolf.java
+++ OOD_Assignment_2/Part_2/Animal/Canine/Wolf.java
@@ -1,1 +1,24 @@
+/*
+ * Author: Caitlin Hagler
+ * Class: Wolf
+ * Purpose: Represents the Wolf animal type.
+ * Last Revision Date: September 2, 2026
+ */
 
+public class Wolf extends Canine {
+
+    public Wolf() {
+        super(Size.LARGE, "Wolf");
+    }
+
+    /*
+     * Wolf-specific sound.
+     */
+  
+    @Override
+    public void makeSound() {
+        System.out.println(
+                getName() + " makes a sound: The wolf howls."
+        );
+    }
+}
