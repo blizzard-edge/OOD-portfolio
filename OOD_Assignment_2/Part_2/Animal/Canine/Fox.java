a06e4bf3896a331b94a148d88c1a72f8168696c5
Blizzard <278835811+blizzard-edge@users.noreply.github.com>
2026-09-02 15:09:16 -0400

    Create Fox class with sound implementation
    Added a Fox class that extends Canine and implements a specific sound.

--- OOD_Assignment_2/Part_2/Animal/Canine/Fox.java
+++ OOD_Assignment_2/Part_2/Animal/Canine/Fox.java
@@ -1,1 +1,24 @@
+/*
+ * Author: Caitlin Hagler
+ * Class: Fox
+ * Purpose: Represents the Fox animal type.
+ * Last Revision Date: September 2, 2026
+ */
 
+public class Fox extends Canine {
+
+    public Fox() {
+        super(Size.MEDIUM, "Fox");
+    }
+
+    /*
+     * Fox-specific sound.
+     */
+  
+    @Override
+    public void makeSound() {
+        System.out.println(
+                getName() + " makes a sound: The fox yips."
+        );
+    }
+}
